f654e0fecb503474666938fd71dc3df762f42e13
TXTuser <gimosteg666@gmail.com>
2024-05-23 16:06:20 +0300

    #fix-bags

--- src/comp/ClassComponent.jsx
+++ src/comp/ClassComponent.jsx
@@ -4,19 +4,19 @@ export default class ClassComponent extends Component {
   render() {
     return (
       <div>
+        {/* For this example to work, you need to pass props to the App.jsx component */}
         {/* <h1 className="solutionONE">
-        For this example to work, you need to pass props to the App.jsx component
           Hello, {this.props.name}, you got the number - {this.props.number}
         </h1> */}
 
+        {/* For this example to work, you need to uncomment line 20 and remove all props from App.jsx */}
         {/* <h1 className="solutionTWO">
-          For this example to work, you need to uncomment line 20 and remove all props from App.jsx
-          Hello, {this.props.name}
+         Hello, {this.props.name}
         </h1> */}
 
+        {/* For this example to work, you need to pass props with an array to the App.jsx file  */}
         {/* <h1>
-          For this example to work, you need to pass props with an array to the
-          App.jsx file Hello, {this.props.name}, you got the number -
+        Hello, {this.props.name}, you got the number -
           {this.props.numbers.join(".")}
         </h1> */}
       </div>
